0ac347310f7d2f5e14000aeb8159cf514fc293a3
b-q <bingzhi.qiao@gmail.com>
2013-05-18 17:05:23 +0200

    for pull

--- src/org/energydata/servlets/Servlet1.java
+++ src/org/energydata/servlets/Servlet1.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 public class Servlet1 extends HttpServlet {
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
     	this.getServletContext().getRequestDispatcher( "/WEB-INF/test.jsp" ).forward( request, response );
